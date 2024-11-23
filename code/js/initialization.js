51cc442391e0379c72caa4aed20a84aeb56b8c93
timfie <111653021+xTimfie@users.noreply.github.com>
2024-11-23 23:37:19 +0100

    Delete code/js/initialization.js

--- code/js/initialization.js
+++
@@ -1,7 +0,0 @@
-import { loadPlaceholder } from './uiUtilities.js';
-import { displayImageBackground } from './uiUtilities.js';
-
-window.onload = function() {
-  loadPlaceholder();
-  displayImageBackground('#888869');
-};
